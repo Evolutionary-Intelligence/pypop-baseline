be0dc9c7ef644fcacec997bd4fb738a079b25fe4
Evolutionary-Intelligence <78018333+Evolutionary-Intelligence@users.noreply.github.com>
2021-11-13 16:31:27 +0800

    Add test_esa.py

---
+++ optimizers/esa/test_esa.py
@@ -0,0 +1,34 @@
+import unittest
+import time
+import numpy as np
+
+from base_functions import rosenbrock
+from esa import ESA as Solver
+
+
+class TestESA(unittest.TestCase):
+    def test_optimize(self):
+        start_run = time.time()
+        ndim_problem = 1000
+        for f in [rosenbrock]:
+            print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
+            problem = {'fitness_function': f,
+                       'ndim_problem': ndim_problem,
+                       'lower_boundary': -5 * np.ones((ndim_problem,)),
+                       'upper_boundary': 5 * np.ones((ndim_problem,))}
+            options = {'max_function_evaluations': 4e5,
+                       'fitness_threshold': 1e-10,
+                       'seed_rng': 0,
+                       'x': 4 * np.ones((ndim_problem,)),
+                       'v': np.ones((ndim_problem,)),
+                       'verbose_frequency': 1,
+                       'record_fitness': True,
+                       'record_fitness_frequency': 50000}
+            solver = Solver(problem, options)
+            results = solver.optimize()
+            print(results)
+            print('*** Runtime: {:7.5e}'.format(time.time() - start_run))
+
+
+if __name__ == '__main__':
+    unittest.main()
